90bf7015d2028d8184c3e41088857f73211d4ae9
Diego Sampaio <chinello@gmail.com>
2018-01-30 11:01:17 -0200

    Add Dockerfile

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+ssh-keygen -A
+
+if [ "${PRIVATE_SSH_KEY}" != "**None**" ]; then
+	echo "=> Found private key"
+
+	# Create SSH files on container
+	mkdir -p /root/.ssh
+	chmod 700 /root/.ssh
+	touch /root/.ssh/id_rsa
+	chmod 600 /root/.ssh/id_rsa
+
+	IFS=$'\n'
+	arr=$(echo ${PRIVATE_SSH_KEY} | tr "," "\n")
+	for x in $arr
+	do
+		x=$(echo $x | sed -e 's/^ *//' -e 's/ *$//')
+		echo "$x" >> /root/.ssh/id_rsa
+	done
+
+	exec ssh -q -4 -oStrictHostKeyChecking=no -L 0.0.0.0:$TUNELOPTS -N $SSHTO
+else
+	echo "ERROR: No private keys found in \$PRIVATE_SSH_KEY"
+	exit 1
+fi
